4c827027035ad34ca06cdcb7199aa38829dd6770
Kwintessencia <alex.sukhanova92@gmail.com>
2023-01-08 19:59:10 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    n = n.toString();
+   let i = n.length -1;
+   let str = '';
+   while (i >=0){
+    str = str + n[i];
+    i--;
+   }
+   return str;
 }
